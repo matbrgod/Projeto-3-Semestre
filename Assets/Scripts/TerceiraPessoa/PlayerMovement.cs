dbe0bc38eff6df3433bfef7f6ac04f92fa2b00b4
yuriyurui <yuritelisgarcia@gmail.com>
2026-03-28 18:15:11 -0300

    inicio da programação do movimento de personagem

---
+++ Assets/Scripts/TerceiraPessoa/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 moveDirection;
+
+    Transform cameraObj;
+    InputManager inputManager;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<InputManager>();
+    }
+
+    public void HandleMovement()
+    {
+        moveDirection = cameraObj.forward * inputManager.moveInput;
+    }
+}
